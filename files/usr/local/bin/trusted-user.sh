57744bf732435f84bd0f6e34b340dfce6155b503
Max <max@maxf.net>
2024-05-09 01:30:29 -0400

    updated

--- files/usr/local/bin/trusted-user.sh
+++ files/usr/local/bin/trusted-user.sh
@@ -1,10 +1,6 @@
 #!/bin/ash -e
 
-sshd_pid=$(ps | grep '^\s*[0-9]*\s*'$LOGNAME | grep -v grep | grep "sshd: $LOGNAME@${SSH_TTY/\/dev\/}\$" | awk '{ print $1 }')
-sshd_ppid=$(grep -o 'sshd\[[0-9]*]: User child is on pid '$sshd_pid /var/log/messages)
-sshd_ppid=${sshd_ppid##*[}
-sshd_ppid=${sshd_ppid%%]*}
-user=$(grep -o 'sshd\['$sshd_ppid']: Accepted publickey for .*' /var/log/messages|sed 's/^.*ID \([^ ]*\) .*/\1/g'|tr '[A-Z]' '[a-z]')
+user=$(cat $SSH_USER_AUTH | cut -d' ' -f2- | ssh-keygen -L -f - | grep '^ *Key ID:' | cut -d'"' -f2 | tr '[A-Z]' '[a-z]')
 
 grep -q ^$user: /etc/passwd || sudo adduser -D -G wheel $user
 
